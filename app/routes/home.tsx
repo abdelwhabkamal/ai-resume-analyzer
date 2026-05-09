e79eaf6f35fe38d750d2caf043e99e314a239510
abdelwhabkamal <abdelwhabkamal17@gmail.com>
2026-05-09 07:54:36 +0300

    feat: implement resume analysis components, data types, and AI feedback constants

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,31 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { useState } from "react";
+import { Navbar } from "../Components/navbar";
+import ResumeCard from "../Components/ResumeCard";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "AI-powered resume analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review and improve your resume with AI</h2>
+      </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+  </main>
 }
